f6e032891e7882b9cddf6325261473eded1dc221
birnl154 <birnl154@newschool.edu>
2023-02-21 08:50:05 -0500

    arrows

--- studio.html/studio.js
+++ studio.html/studio.js
@@ -60,17 +60,3 @@ button.addEventListener('click', function(){
 
     outputDiv.innerHTML = `<h3>${count}</h3>`
 });
-
-document
-.querySelector("#word")
-.addEventListener("input", function countWord() {
-  let res = [];
-  let str = this.value.replace(/[\t\n\r\.\?\!]/gm, " ").split(" ");
-  str.map((s) => {
-    let trimStr = s.trim();
-    if (trimStr.length > 0) {
-      res.push(trimStr);
-    }
-  });
-  document.querySelector("#show").innerText = res.length;
-});

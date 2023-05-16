aa5e95bf5591c0f6a38a4dfc471d7ceec56332cf
birnl154 <birnl154@newschool.edu>
2023-05-16 11:21:15 -0400

    json assignment fixed

--- jsondemo.js
+++ jsondemo.js
@@ -1,5 +1,33 @@
-function showList(){
-    fetch("class.json")
-      .then(response => response.json())
-      .then(data => createList(data));
+// Function to display JSON data in a list
+function displayData() {
+  var dataList = document.getElementById("dataList");
+  dataList.innerHTML = ""; // Clear the list before displaying new data
+
+  // Fetch the JSON file
+  fetch('class.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(jsonData) {
+      // Loop through the JSON data and create list items
+      jsonData.forEach(function(item) {
+        var nameItem = document.createElement("li");
+        nameItem.innerText = "Name: " + item.Name;
+        dataList.appendChild(nameItem);
+
+        var githubItem = document.createElement("li");
+        var githubLink = document.createElement("a");
+        githubLink.href = "https://" + item.Github + ".github.io";
+        githubLink.innerText = "https://" + item.Github + ".github.io";
+        githubItem.appendChild(githubLink);
+        dataList.appendChild(githubItem);
+      });
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
 }
+
+// Attach click event listener to the button
+var showDataButton = document.getElementById("showDataButton");
+showDataButton.addEventListener("click", displayData);
